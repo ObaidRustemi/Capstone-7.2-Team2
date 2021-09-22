676aae982956242a318c0ccb5b9b9972da767725
Obaid Rustemi <orustemi@gmail.com>
2021-09-21 20:38:10 -0400

    tweaks to user routing container

--- front-end/src/Containers/VenueOwnerContainer.js
+++ front-end/src/Containers/VenueOwnerContainer.js
@@ -5,6 +5,7 @@ import { apiURL } from "../util/apiURL";
 import { getUser, getVenues } from "../Actions/userActions";
 
 import VenueShow from "../Pages/VenueShow";
+import ArtistShow from "../Pages/ArtistShow";
 import axios from "axios";
 
 const API = apiURL();
@@ -13,25 +14,23 @@ const VenueOwnerContainer = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   // const user = useSelector((state) => state.user);
-  const venues = useSelector((state) => state.venues)
-
+  const venues = useSelector((state) => state.venues);
 
   useEffect(() => {
     const fetchVenuesForUser = async () => {
       try {
         let res = await axios.get(`${API}/users/${id}/venues`);
-      
+
         const action = getVenues(res.data.payload);
-      
+
         dispatch(action);
       } catch (error) {
         console.log(error);
       }
     };
-    fetchVenuesForUser()
+    fetchVenuesForUser();
   }, []);
-
-  return <VenueShow venues={venues} />
+  return venues.length === 0 ? <ArtistShow /> : <VenueShow venues={venues} />;
 };
 
 export default VenueOwnerContainer;

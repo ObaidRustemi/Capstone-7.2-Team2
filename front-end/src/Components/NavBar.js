82d051b8c654ac6c541e36fa74dd1521a832edb2
Carlo <carlonyc001@gmail.com>
2021-09-23 20:30:46 -0400

    navbar styling

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -2,6 +2,7 @@ import "../Styling/NavBar.css";
 import { NavLink, Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import { useAuth } from "../Contexts/AuthContext";
+import "../index.css"
 
 const NavBar = () => {
   const { currentUser } = useAuth();
@@ -9,7 +10,10 @@ const NavBar = () => {
   return (
     <div id="nav-main-div">
       <div class="left" id="title">
-        <Link to="/">OURArt</Link>
+        <Navbar.Brand href="/">
+          onlyART
+        </Navbar.Brand>
+        {/* <Link to="/">onlyART</Link> */}
       </div>
       <div class="right" id="links">
         {currentUser ? (
@@ -18,7 +22,7 @@ const NavBar = () => {
             <NavLink to="/dashboard"> Account Info </NavLink>
           </div>
         ) : (
-          <div>
+          <div className="nav" >
             <p>
               <NavLink to="/signup"> Register </NavLink>
             </p>

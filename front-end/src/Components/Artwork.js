af6c6bf3e051baa86495fbae6c79088f3dc3af43
Flower Aviles <itsfloweraviles@gmail.com>
2021-09-25 01:24:00 -0400

    updated artwork.js file

--- front-end/src/Components/Artwork.js
+++ front-end/src/Components/Artwork.js
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 const ArtworkListItem = ({ artwork }) => {
     return (
         <li>
-            <Link to={`/artwork/${artwork.id}`}>
+            <Link to={`/users/${artwork.artist_id}/artwork/${artwork.id}`}>
                 <img src={artwork.image} alt="artwork img" />
+                    <h2>{artwork.title}</h2>
             </Link>
         </li>
     );

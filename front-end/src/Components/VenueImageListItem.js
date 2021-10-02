56c56321d73cf1c643de650292926de6831bd8ac
Obaid Rustemi <orustemi@gmail.com>
2021-10-01 22:15:13 -0400

    add venue modal and animations

--- front-end/src/Components/VenueImageListItem.js
+++ front-end/src/Components/VenueImageListItem.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import "../Styling/VenueInfo.css";
+import { motion } from 'framer-motion'
 
-const VenueImageListItem = ({image}) => {
+const VenueImageListItem = ({image, setSelectedImg}) => {
+  
+
     return (
-        <div>
-          <img  src={image.photo_url}/>  
-        </div>
+        <motion.div className="img-wrap-div" key={image.id}
+        layout
+        whileHover ={{opacity: 1}}
+        onClick={() => setSelectedImg(image.photo_url)}
+        >
+          <motion.img  src={image.photo_url}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{delay: 0.75}}
+          />  
+        </motion.div>
     )
 }
 

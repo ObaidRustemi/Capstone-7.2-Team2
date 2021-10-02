7e1c608891616d03bd4c063b7bf02184131959a7
Obaid Rustemi <orustemi@gmail.com>
2021-10-02 17:59:57 -0400

    artist show progress

--- front-end/src/Containers/UserRoutingContainer.js
+++ front-end/src/Containers/UserRoutingContainer.js
@@ -2,21 +2,23 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import { apiURL } from "../util/apiURL";
-import { getUser, getUserVenues, getArtworks } from "../Actions/userActions";
+import { getUserVenues, getUserArtwork, getUser } from "../Actions/userActions";
 
 import VenueShow from "../Pages/VenueShow";
 import ArtistShow from "../Pages/ArtistShow";
 import axios from "axios";
+import "../Styling/ArtworkIndex.css";
 
 const API = apiURL();
 
 const UserRoutingContainer = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
-  // const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user);
   const venues = useSelector((state) => state.venues);
   const artwork = useSelector((state) => state.artwork);
   const [isVenue, setIsVenue] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -28,29 +30,30 @@ const UserRoutingContainer = () => {
         // passing down venues state into it
         if (res.data.payload.venues) {
           await setIsVenue(true);
-
+          setCurrentUser(res.data.payload.user);
           const action = getUserVenues(res.data.payload.venues);
           dispatch(action);
         } else {
           await setIsVenue(false);
-          if (artwork.length) {
-            const action = getArtworks(res.data.payload.userArtwork);
-
-            dispatch(action);
-          }
+          setCurrentUser(res.data.payload.user);
+          const action = getUserArtwork(res.data.payload.userArtwork);
+          const action2 = getUser(res.data.payload)
+          dispatch(action);
+          dispatch(action2);
         }
       } catch (error) {
         console.log(error);
       }
     };
-
     fetchUser();
   }, []);
 
   return isVenue === false ? (
-    <ArtistShow artwork={artwork} />
+    <div className="artist-show-page">
+      <ArtistShow currentUser={currentUser} artwork={artwork} />
+    </div>
   ) : (
-    <VenueShow venues={venues} />
+    <VenueShow currentUser={currentUser} venues={venues} />
   );
 };
 
--- front-end/src/Reducers/venuesReducer.js
+++ front-end/src/Reducers/venuesReducer.js
@@ -3,7 +3,7 @@ import { GET_VENUES, GET_VENUE, GET_USER_VENUES } from "../Actions/actionTypes";
 const initialState = [];
 
 const venuesReducer = (state = initialState, action) => {
-  debugger
+ 
   console.log("TOP LEVEL IN VENUES REDUCER");
   switch (action.type) {
     case GET_VENUES:
@@ -14,8 +14,8 @@ const venuesReducer = (state = initialState, action) => {
       console.log("GET_VENUE case");
       return [...state, action.payload.user];
     case GET_USER_VENUES:
-      debugger
-      console.log("GET_USER_VENUE case");
+   
+      console.log("GET_USER_VENUES case");
       return action.payload;
     default:
       return state;

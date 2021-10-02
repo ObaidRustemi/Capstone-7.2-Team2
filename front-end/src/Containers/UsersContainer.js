1e3ea35dd64b13ac744c79bf679f31f568c19605
Carlo <carlonyc001@gmail.com>
2021-10-01 22:22:34 -0400

    users list link

--- front-end/src/Containers/UsersContainer.js
+++ front-end/src/Containers/UsersContainer.js
@@ -1,12 +1,87 @@
+// import React, { useEffect, useState } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { apiURL } from "../util/apiURL";
+// import { getUsers } from "../Actions/userActions";
+// import UserIndex from "../Pages/UserIndex";
+// import axios from "axios";
+// import { Button } from "react-bootstrap";
+// import Switch from "../Components/Switch"
+// import "../Styling/Switch.css"
+
+// const API = apiURL();
+
+// const UsersContainer = () => {
+//   const users = useSelector((state) => state.users);
+//   const dispatch = useDispatch();
+//   const [toggled, setToggled] = useState(false);
+//   // const [isToggled, setIsToggled] = useState(false);
+
+//   useEffect(() => {
+//     const fetchUsers = async () => {
+//       try {
+//         let res = await axios.get(`${API}/users`); 
+//         const action = getUsers(res.data.payload);
+//         console.log("about to dispatch:::", action);
+//         dispatch(action);
+
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     if(users.length === 0){
+//       fetchUsers();
+//     }
+//   }, []);
+  
+
+//   //  const handleCheckboxChange = async () => {
+//   //   let res = await axios.get(`${API}/users`); 
+//   //       const action = getUsers(res.data.payload.filter((item) => item.is_venue));
+//   //       debugger;
+//   //       console.log("about to dispatch:::", action);
+//   //       dispatch(action);
+//   // };
+
+//   // const handleClick = () => {
+//   //   setToggle({toggle: !toggle})
+//   // }
+
+//   // const handleToggle = () => {
+//   //   setIsToggled({isToggled: !isToggled})
+//   // }
+
+//   const handleToggle = (e) => {
+//     setToggled(e.target.checked)
+//   }
+
+//   const venues = users.filter(user => user.is_venue)
+//   const artists = users.filter(user => user.is_artist)
+
+//   ///useState for user to render 
+//   //make toggle instead 
+//   return (
+//     <div>
+//       {/* {toggle && <>Aritsit</>} */}
+//       {/* <Button onClick={handleClick}>Venues </Button> */}
+//     <Switch users={users} onChange={handleToggle}/>
+//     <p>{toggled ? <ul>{artists.map(venue => <li>{venue.username}</li>)}</ul> :  <ul>{venues.map(venue => <li>{venue.username}</li>)}</ul>}</p>
+//       {/* <UserIndex users={users} /> */}
+
+//     </div>
+//   )
+// };
+
+// export default UsersContainer;
+
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { apiURL } from "../util/apiURL";
 import { getUsers } from "../Actions/userActions";
-import UserIndex from "../Pages/UserIndex";
+// import UserIndex from "../Pages/UserIndex";
 import axios from "axios";
-import { Button } from "react-bootstrap";
 import Switch from "../Components/Switch"
 import "../Styling/Switch.css"
+import { Link } from "react-router-dom"
 
 const API = apiURL();
 
@@ -14,7 +89,6 @@ const UsersContainer = () => {
   const users = useSelector((state) => state.users);
   const dispatch = useDispatch();
   const [toggled, setToggled] = useState(false);
-  // const [isToggled, setIsToggled] = useState(false);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -23,32 +97,14 @@ const UsersContainer = () => {
         const action = getUsers(res.data.payload);
         console.log("about to dispatch:::", action);
         dispatch(action);
-
       } catch (error) {
         console.log(error);
       }
     };
-    if(users.length === 0){
+    if (users.length === 0) {
       fetchUsers();
     }
   }, []);
-  
-
-  //  const handleCheckboxChange = async () => {
-  //   let res = await axios.get(`${API}/users`); 
-  //       const action = getUsers(res.data.payload.filter((item) => item.is_venue));
-  //       debugger;
-  //       console.log("about to dispatch:::", action);
-  //       dispatch(action);
-  // };
-
-  // const handleClick = () => {
-  //   setToggle({toggle: !toggle})
-  // }
-
-  // const handleToggle = () => {
-  //   setIsToggled({isToggled: !isToggled})
-  // }
 
   const handleToggle = (e) => {
     setToggled(e.target.checked)
@@ -56,17 +112,13 @@ const UsersContainer = () => {
 
   const venues = users.filter(user => user.is_venue)
   const artists = users.filter(user => user.is_artist)
-
-  ///useState for user to render 
-  //make toggle instead 
+ 
   return (
     <div>
-      {/* {toggle && <>Aritsit</>} */}
-      {/* <Button onClick={handleClick}>Venues </Button> */}
     <Switch users={users} onChange={handleToggle}/>
-    <p>{toggled ? <ul>{artists.map(venue => <li>{venue.username}</li>)}</ul> :  <ul>{venues.map(venue => <li>{venue.username}</li>)}</ul>}</p>
-      {/* <UserIndex users={users} /> */}
-
+    <p>{toggled 
+        ? <ul>{artists.map(artist => <li><Link to={`/users/${artist.id}`}>{artist.username}</Link></li>)} </ul>
+        :  <ul>{venues.map(venue => <li><Link to={`/users/${venue.id}`}>{venue.username}</Link></li>)}</ul>}</p>
     </div>
   )
 };

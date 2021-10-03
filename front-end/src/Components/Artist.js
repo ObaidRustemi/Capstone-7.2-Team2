42f7f0f7a7d121c34e535c67bc59472132802d4b
Jonathan B <75285297+bedonjonathan@users.noreply.github.com>
2021-10-03 16:01:03 -0400

    correct url for front end (#32)

--- front-end/src/Components/Artist.js
+++ front-end/src/Components/Artist.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useSelector } from "react-redux"
 import axios from "axios";
 import { apiURL } from "../util/apiURL";
 import { useParams } from "react-router-dom";
@@ -9,10 +10,11 @@ const Artist = () => {
   const [artist, setArtist] = useState({});
   const [artwork, setArtwork] = useState({});
   const { id } = useParams();
-
+  const currentUser = useSelector((state)=>state.currentUser)
+  const firebase_uid = currentUser.user.uid
   const fetchArtist = async () => {
     try {
-      const artistRes = await axios.get(`${API}/users/${id}`);
+      const artistRes = await axios.get(`${API}/users/${firebase_uid}`);
       setArtist(artistRes.data);
     } catch (error) {
       console.log(error);
@@ -21,7 +23,7 @@ const Artist = () => {
 
   const fetchArtwork = async () => {
     try {
-      const artworkRes = await axios.get(`${API}/users/${id}/artwork`);
+      const artworkRes = await axios.get(`${API}/users/${firebase_uid}/artwork`);
 
       setArtwork(artworkRes.data);
     } catch (error) {

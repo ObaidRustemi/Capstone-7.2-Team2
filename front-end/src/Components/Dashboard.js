7548c664a639d1208a159b1307457c3c46593355
Carlo <carlonyc001@gmail.com>
2021-10-06 16:18:42 -0400

    useeffect test

--- front-end/src/Components/Dashboard.js
+++ front-end/src/Components/Dashboard.js
@@ -1,20 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../Contexts/AuthContext";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useHistory, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { apiURL } from "../util/apiURL";
+
+const API = apiURL();
 
 export default function Dashboard() {
   const [error, setError] = useState("");
   const { logout } = useAuth();
   const currentUser = useSelector((state) => state.currentUser);
   const history = useHistory();
+  const { id } = useParams();
 
-  if(currentUser) {
-    debugger
-    console.log(currentUser.displayName)
-    debugger
-  }
+  // if(currentUser) {
+  //   debugger
+  //   console.log(currentUser.displayName)
+  //   debugger
+  // }
+
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await currentUser.username
+        debugger;
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchUser()
+  }, [])
+  
 
   async function handleLogout() {
     setError("");

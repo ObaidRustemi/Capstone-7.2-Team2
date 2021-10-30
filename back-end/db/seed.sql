8eb8f6024f9580e0d5cb03365025e457e180fa7c
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-10-30 12:15:48 -0400

    artist seed data updated

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -2,14 +2,20 @@
 
 
 INSERT INTO users(username, firebase_uid, image, type_of_art, description, phone_number, location, is_venue, is_artist) VALUES
-('Kaws', 'PYKsxQioabZRlt0f0aHtAu6tl1j1', 'https://www.christies.com/media-library/images/features/articles/2019/03/19/10-things-to-know-about-kaws/kaws_briandonnelly_by_hye-ryoungmin-hero-880i.jpg', 'Pop Art', 'Brian Donnelly, known professionally as Kaws, is an American artist and designer.', 12345678, 'NY,USA', false, true),
-('Takashi Murakami','rKuKNL30PYVxQDn5yLDliTWmjCf2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS67Y4gvW3UQbpAWcTafjzigvRjy_7pDj0YTA&usqp=CAU', 'Contemporary', 'Takashi Murakami is a Japanese contemporary artist. He works in fine arts media as well as commercial and is known for blurring the line between high and low arts.', 12345678, 'Tokyo, Japan', false, true),
-('Gasebo', 'nURYZDadw2aZ1ESF6zoaHdYnSv32', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxMt5zjsNv8tT-SqH1kMxn4N6hfwdOhAMogw&usqp=CAU', 'Realism', 'Realism is a genre of art which is based on practical matters or real things rather than imaginary or visionary representations. Realism developed as a reaction to the romanticism in the 19th century. ', 12345678, 'PAT,NJ', false, true),
+-- ('Kaws', 'PYKsxQioabZRlt0f0aHtAu6tl1j1', 'https://www.christies.com/media-library/images/features/articles/2019/03/19/10-things-to-know-about-kaws/kaws_briandonnelly_by_hye-ryoungmin-hero-880i.jpg', 'Pop Art', 'Brian Donnelly, known professionally as Kaws, is an American artist and designer.', 12345678, 'NY,USA', false, true),
+-- ('Takashi Murakami','rKuKNL30PYVxQDn5yLDliTWmjCf2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS67Y4gvW3UQbpAWcTafjzigvRjy_7pDj0YTA&usqp=CAU', 'Contemporary', 'Takashi Murakami is a Japanese contemporary artist. He works in fine arts media as well as commercial and is known for blurring the line between high and low arts.', 12345678, 'Tokyo, Japan', false, true),
+-- ('Gasebo', 'nURYZDadw2aZ1ESF6zoaHdYnSv32', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxMt5zjsNv8tT-SqH1kMxn4N6hfwdOhAMogw&usqp=CAU', 'Realism', 'Realism is a genre of art which is based on practical matters or real things rather than imaginary or visionary representations. Realism developed as a reaction to the romanticism in the 19th century. ', 12345678, 'Paterson, NJ', false, true),
 ('MearOne', 'CUotx1PC0NaAlDkoAQGPE7DD8jb2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsPOjdZEFuf0XsPl9nBVP4MHlgdRdrH6hbdw&usqp=CAU', 'Visionary', 'Mear One is an American artist based in Los Angeles, known for his often-political street graffiti art.', 12345678, 'NY,USA', false, true),
-('Frankie','LydaEG5k94TfDmT9KIKAhdzG9Tx2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkuhfsoNiO7xvIPlUm9F6YlSkbg5TIMKPzsQ&usqp=CAU', 'Romanticism', 'Romanticism is a genre of art which was originated in Europe somewhere in the 18th century against the Age of Enlightenment. Romanticism was an artistic and intellectual movement which is describes by an elevated interest in nature, importance given to the individual’s expression of thoughts and feelings, and to reject the norms of classicism', 12345678, 'NY,USA', false, true),
-('Mike', 'ZE6t5nOcxwMMhFxtaanEMBSWlYn2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkBjPgf2fnrs1xijdTGBEqIVggPnTInj9EPQ&usqp=CAU', 'Art Nouveau', 'Art Nouveau is a term which is derived from French language which literally means ‘new art’. This genre of art developed in France from where it extended to different European countries such as Belgium, Germany, Italy, etc.', 12345678, 'NY,USA', true, false), 
-('Sam','SItsbxhHWLTDxvQ1wrQQwnSlnES2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Zs0Xktv98InMPo4XQUeL6IK8BvsqkSnYgw&usqp=CAU', 'Expressionism', 'Expressionism is a genre of art which deals with the subjective feelings of an artist. This genre of art flourished in the 20th century in a response to the impressionism. The main purpose of expressionism is to depict emotions by means of art.', 12345678, 'PAT,NJ', true, false);
-
+('Frankie','LydaEG5k94TfDmT9KIKAhdzG9Tx2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkuhfsoNiO7xvIPlUm9F6YlSkbg5TIMKPzsQ&usqp=CAU', 'Romanticism', 'Romanticism is a genre of art which was originated in Europe somewhere in the 18th century against the Age of Enlightenment. Romanticism was an artistic and intellectual movement which is describes by an elevated interest in nature, importance given to the individual’s expression of thoughts and feelings, and to reject the norms of classicism', 12345678, 'USA', false, true),
+('Mike', 'ZE6t5nOcxwMMhFxtaanEMBSWlYn2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkBjPgf2fnrs1xijdTGBEqIVggPnTInj9EPQ&usqp=CAU', 'Art Nouveau', 'Art Nouveau is a term which is derived from French language which literally means new art’. This genre of art developed in France from where it extended to different European countries such as Belgium, Germany, Italy, etc.', 12345678, 'USA', true, false), 
+('Sam','SItsbxhHWLTDxvQ1wrQQwnSlnES2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Zs0Xktv98InMPo4XQUeL6IK8BvsqkSnYgw&usqp=CAU', 'Expressionism', 'Expressionism is a genre of art which deals with the subjective feelings of an artist. This genre of art flourished in the 20th century in a response to the impressionism. The main purpose of expressionism is to depict emotions by means of art.', 12345678, 'Paterson, NJ', true, false),
+('Rocio Marie Cabrera','x8CeahIL7jaZ3rENptL7d9mY6t52', 'https://images.squarespace-cdn.com/content/v1/567f25e4a976af5d461070fe/1497822525271-6V7XYSC6I1JY1TLV37HV/image-asset.jpeg?format=750w', 'Surreal Pop Art', 'Painter and sculptor from the Bronx', 333-444-7777, 'Bronx, NY', false, true),
+('Annie Legnini','Ua9dUh7IkOQnumORXggQ97IAyis2', 'https://pkimgcdn.peekyou.com/a55e9e1eed3b4cf82c7ff658596eb0d0.jpeg', 'Collage & Mixed Media', 'Creator of Bronx Faces', 444-555-888, 'Bronx, NY', false, true),
+('Monica Hernandez','fjmPsFY8jvPFLX9DsFFQjiiZYVh2', 'https://static.wixstatic.com/media/5d56fa_ed32fca8fa2e4f9ea3c81b1eaa74f55a~mv2.jpg/v1/fill/w_429,h_536,al_c,q_90/IMG_8680.webp', 'Oil Paint & Canvas', 'First Generation Dominican Artist', 444-555-888, 'Bronx, NY', false, true),
+('Fred Emmanuel', 'SSQmKH488PUYED1oJe0MPKidPaE2', 'https://i.imgur.com/5OjHAg1.jpg', 'Pop Art', 'Fred Emmanuelle, known professionally as just FredEE, is an American artist and designer', 123-456-7877, 'Rochester, NY', false, true),
+('Mike Mitchell', 'C8QFbExthBMvPkGZ2CtM13bcWoi1', 'https://geekxlovin.files.wordpress.com/2014/05/this-one-3.jpg', 'Surrealism', 'Mike Mitchell is an American artist known for his pop surrealism', 123-456-7899, 'Los Angeles, CA', false, true),
+('Jonathan Solter', 'lLRNjT58uVQkDPagXaBW1HMdGRk1', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe0ikHZj4pxQ3LJ6Q06deAfajXXNJ4dg0X0w&usqp=CAUP', 'Visionary Art', 'Jonathan Solter is an artist living and working in the Bay Area. Growing up in the redwood forests of Northern California, he started making art at an early age with the loving support of his family. This began with mazes and labyrinths, leading to the creation of new worlds from both his imagination and the natural environment; and turning more towards Psychedelic art after high school', 917-123-4567, 'Oakland, CA', false, true),
+('Alex Polanco', 'wD7INEdpccYJr5ilh2GVGPDZicq2', 'http://alexpolancoart.com/images/IMG_2433.jpg', 'Visionary Art', 'Taino artist, born in Kiskeya (Dominican Republic)', 347-265-5122 , 'New York, NY', false, true);
 
 
 INSERT INTO artwork (title, artist_id,  image) VALUES
@@ -24,14 +30,37 @@ INSERT INTO artwork (title, artist_id,  image) VALUES
 -- ('title 6', 6, 'https://mearone.com/wp-content/uploads/2021/03/TRIPFULL.jpg'),
 -- ('title 7', 7, 'https://cdn.shopify.com/s/files/1/1043/4860/products/14ers_original_batik_12_by_17_300_2048x2048.JPG?v=1455754701');
 
-('title 1', 'PYKsxQioabZRlt0f0aHtAu6tl1j1', 'https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2012/01/hokusai.jpg'),
-('title 2', 'PYKsxQioabZRlt0f0aHtAu6tl1j1', 'https://image.shutterstock.com/image-illustration/seven-running-horses-wildlife-decorative-600w-1754614820.jpg'),
-('title 3', 'rKuKNL30PYVxQDn5yLDliTWmjCf2', 'https://assets.phillips.com/image/upload/t_Website_LotDetailMainImage/v1602889523/auctions/NY010320/8_001.jpg'),
-('title 4', 'rKuKNL30PYVxQDn5yLDliTWmjCf2', 'https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/11/11/1415723375557/9390c5a0-ff8c-4866-be24-8dc86fb512c7-1020x1016.jpeg?width=700&quality=85&auto=format&fit=max&s=4e31578a1b6f94c0328c1d6c5316a8e0'),
+-- ('title 1', 'PYKsxQioabZRlt0f0aHtAu6tl1j1', 'https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2012/01/hokusai.jpg'),
+-- ('title 2', 'PYKsxQioabZRlt0f0aHtAu6tl1j1', 'https://image.shutterstock.com/image-illustration/seven-running-horses-wildlife-decorative-600w-1754614820.jpg'),
+-- ('title 3', 'rKuKNL30PYVxQDn5yLDliTWmjCf2', 'https://assets.phillips.com/image/upload/t_Website_LotDetailMainImage/v1602889523/auctions/NY010320/8_001.jpg'),
+-- ('title 4', 'rKuKNL30PYVxQDn5yLDliTWmjCf2', 'https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/11/11/1415723375557/9390c5a0-ff8c-4866-be24-8dc86fb512c7-1020x1016.jpeg?width=700&quality=85&auto=format&fit=max&s=4e31578a1b6f94c0328c1d6c5316a8e0'),
 ('title 5', 'SItsbxhHWLTDxvQ1wrQQwnSlnES2', 'https://images.squarespace-cdn.com/content/v1/591cf35d20099ed1a3cc9c94/1495073736483-WD16PW5W6Y5QFIOAENL2/XCQ8Qcr.jpg?format=750w'),
 ('Awake', 'CUotx1PC0NaAlDkoAQGPE7DD8jb2', 'https://mearone.com/wp-content/uploads/2021/03/TRIPFULL.jpg'),
 ('Planet X', 'CUotx1PC0NaAlDkoAQGPE7DD8jb2', 'https://i.ytimg.com/vi/3Gf8vz7ZI3c/maxresdefault.jpg'),
-('title 7', 'LydaEG5k94TfDmT9KIKAhdzG9Tx2', 'https://cdn.shopify.com/s/files/1/1043/4860/products/14ers_original_batik_12_by_17_300_2048x2048.JPG?v=1455754701');
+('title 7', 'LydaEG5k94TfDmT9KIKAhdzG9Tx2', 'https://cdn.shopify.com/s/files/1/1043/4860/products/14ers_original_batik_12_by_17_300_2048x2048.JPG?v=1455754701'),
+('Imposter', 'x8CeahIL7jaZ3rENptL7d9mY6t52', 'https://images.squarespace-cdn.com/content/v1/591cf35d20099ed1a3cc9c94/1623360518321-JSZT50V1NGB5KT6VTIMT/imposter.jpg'),
+('Darkness', 'x8CeahIL7jaZ3rENptL7d9mY6t52', 'https://images.squarespace-cdn.com/content/v1/591cf35d20099ed1a3cc9c94/1628462669836-Y77I8WYLBECL8YS1DX50/darkness.jpg'),
+('Feelings are Magic', 'x8CeahIL7jaZ3rENptL7d9mY6t52', 'https://images.squarespace-cdn.com/content/v1/591cf35d20099ed1a3cc9c94/1634682072658-YGV8GRBOWBJIDTT23EIC/rocio.jpg'),
+('Didn’t Come From No Rib', 'Ua9dUh7IkOQnumORXggQ97IAyis2', 'https://images.squarespace-cdn.com/content/v1/56b3ed2cb654f954edaa6c5a/1520570603049-GZD7FUPM6O209RALOKM2/DIDNT+COME+FROM+NO+RIB+final.jpeg'),
+('Untitled', 'Ua9dUh7IkOQnumORXggQ97IAyis2', 'https://images.squarespace-cdn.com/content/v1/56b3ed2cb654f954edaa6c5a/1476230231232-8LI8R7MV46BYK1OZRA26/10881653_10205370638689319_5516323048521139071_n.jpg'),
+('Bodega Boys', 'Ua9dUh7IkOQnumORXggQ97IAyis2', 'https://images.squarespace-cdn.com/content/v1/56b3ed2cb654f954edaa6c5a/1491239973949-42T8IY3C4FTQE8QN6CD9/Desus+and+Mero+mixed+media.JPG'),
+('Jengibre', 'fjmPsFY8jvPFLX9DsFFQjiiZYVh2', 'https://static.wixstatic.com/media/5d56fa_3a3a2f30cc34448c890d5f3724b8b014~mv2.jpg/v1/fill/w_494,h_605,al_c,q_90/5d56fa_3a3a2f30cc34448c890d5f3724b8b014~mv2.jpg'),
+('I want a house', 'fjmPsFY8jvPFLX9DsFFQjiiZYVh2', 'https://static.wixstatic.com/media/5d56fa_7a3108dde9ab49639f5df72a2b5ef875~mv2_d_2621_3930_s_4_2.jpg/v1/fill/w_494,h_740,al_c,q_90/5d56fa_7a3108dde9ab49639f5df72a2b5ef875~mv2_d_2621_3930_s_4_2.jpg'),
+('Are you okay?', 'fjmPsFY8jvPFLX9DsFFQjiiZYVh2', 'https://static.wixstatic.com/media/5d56fa_08fe1f4b56c54a749b0c82ac1bc5d513~mv2.jpeg/v1/fill/w_494,h_744,al_c,q_90/5d56fa_08fe1f4b56c54a749b0c82ac1bc5d513~mv2.jpeg'),
+('covide 19', 'SSQmKH488PUYED1oJe0MPKidPaE2', 'https://lh3.googleusercontent.com/a5145CjqO3BtSpr70bBCrCiaDMaLUI5U8MJlBgreNY5wYRDBVOOLhM8112Dub8n2ys4lKUbFmCK9nvZ8MdbJFBtqQQ8BRZPOPt2t=w600'),
+('woman of dignity', 'SSQmKH488PUYED1oJe0MPKidPaE2', 'https://lh3.googleusercontent.com/uonorn3L54s7QU0BC1_1K-sEelR1m1bYwbfZIu6hNAofr8sXiQ3if9d4uyHd7byf37JUhv2_-tkSb3EjKlFiEtPGKuCKhxdksJldDg=w600'),
+('XxxTentacion', 'SSQmKH488PUYED1oJe0MPKidPaE2', 'https://lh3.googleusercontent.com/oboVFs7KKYBB52oBI1TM0M47EBnvpmLxOjHf9MAt80X5P2v1oxIpMc2Q9EC3eOQDmHwvz00Q-Z5I1TkNCbIxzg_bHToVKo-LbNnl=w600'),
+('Made in America', 'C8QFbExthBMvPkGZ2CtM13bcWoi1', 'https://i.imgur.com/DvqmxBQ.jpeg'),
+('Django', 'C8QFbExthBMvPkGZ2CtM13bcWoi1', 'https://i.imgur.com/Jh9u8cz.jpeg'),
+('Gourmet Coffee', 'C8QFbExthBMvPkGZ2CtM13bcWoi1', 'https://i.imgur.com/pHnMD2h.jpeg'),
+('Urban Fungus', 'lLRNjT58uVQkDPagXaBW1HMdGRk1', 'https://cdncozygallery.addons.business/8330/181126091119lc1611d6d-xs.jpg?c=10'),
+('Moon Goddess', 'lLRNjT58uVQkDPagXaBW1HMdGRk1', 'https://cdncozygallery.addons.business/8330/181126091116lc1611gtt-xs.jpg?c=10'),
+('Northern Light', 'lLRNjT58uVQkDPagXaBW1HMdGRk1', 'https://cdncozygallery.addons.business/8330/181126091115lc1611iJn-xs.jpg?c=10'),
+('Bliss', 'lLRNjT58uVQkDPagXaBW1HMdGRk1', 'https://cdncozygallery.addons.business/8330/181126091108lc1611VqE-xs.jpg?c=10'),
+('Origins', 'wD7INEdpccYJr5ilh2GVGPDZicq2', 'http://alexpolancoart.com/images/Origins_Preview.jpg'),
+('Divine Being', 'wD7INEdpccYJr5ilh2GVGPDZicq2', 'http://alexpolancoart.com/images/DivineBeing_Preview.jpg'),
+('Conscious Awareness', 'wD7INEdpccYJr5ilh2GVGPDZicq2', 'http://alexpolancoart.com/images/Cawareness_Preview.jpg'),
+('Vision of the Light', 'wD7INEdpccYJr5ilh2GVGPDZicq2', 'http://alexpolancoart.com/images/VisionOfTheLight_2x.jpg');
 
 
 INSERT INTO venues ( name, venue_profile_photo, owner_id, venue_info, address) VALUES  
